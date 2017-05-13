61c773a526b56e9163109f73ad5b86d1bee8171a
pavelmaks1 <pavel.maks7@yandex.ru>
2017-05-13 20:35:55 +0200

    additional fields and profile page added

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,5 +6,7 @@ class StaticPagesController < ApplicationController
   def about
   end
 
+  def profile
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' }
   root 'static_pages#index'
 
   get 'about', to: 'static_pages#about'
+  get 'profile', to: 'static_pages#profile'
+  get 'sign_out', to: 'devise#sign_out'
 end
